d5fee9eaf04bed58e1d68715d996854a637f06e4
Teodora <jwirss32@gmail.com>
2026-04-11 17:43:22 +0300

    filter by user added

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,35 @@ import {useState, useEffect} from 'react'
 
 function App() {
   const [todos, setTodos] = useState([])
+  const [users, setUsers] = useState([])
+  const [filterUserId, setFilterUserId] = useState('all')
 
   //reading and displaying info from api
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/todos')
     .then(response => response.json())
     .then(data => setTodos(data))
+
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(data => setUsers(data))
   }, [])
 
-  // return (
-  //   <ul>
-  //     {todos.map(todo => (
-  //       <li key={todo.id}>{todo.title}</li>
-  //     ))}
-  //   </ul>
-  // )
-    
+  //combine filters
+  const filteredTodos = filterUserId === 'all'
+    ? todos
+    : todos.filter(todo => todo.userId.toString() === filterUserId)
+  
+  //using for fact-checking might remove later
+  const getUserName = (userId) => {
+    const user = users.find(user => user.id === userId)
+    return user ? user.name : '${userId}'
+  }
+
   //creating consts to filter through the todos
-  const completedTodos = todos.filter(todo => todo.completed)
-  const uncompletedTodos = todos.filter(todo => !todo.completed)
+  const completedTodos = filteredTodos.filter(todo => todo.completed)
+  const uncompletedTodos = filteredTodos.filter(todo => !todo.completed)
+
 
   //button for moving tasks
   const moveTodo = (id) => {
@@ -36,23 +46,45 @@ function App() {
     setTodos(updatedTodos)
   }
 
+  const handleFilterChange = (userId) => {
+    setFilterUserId(userId)
+  }
+
   return (
-    <div className ="flex-container">
+    <div>
+      <div>
+        <label>Filter by:</label>
+        <select value={filterUserId} onChange={(e) => handleFilterChange(e.target.value)}>
+          <option value="all">All</option>
+          {users.map(user => (
+            <option key={user.id} value={user.id}>
+            {user.name}
+          </option>
+          ))}          
+        </select>
+      </div>
+
+      <div className ="flex-container">
       <div className="flex-item">
         <h1>uncomplete todos</h1>
         {uncompletedTodos.map(todo => (
-          <div>{todo.title}
-          <button onClick={() => moveTodo(todo.id)}>Complete</button>
+          <div key={todo.id} className="todo-item">
+            <span>{getUserName(todo.userId)}: </span>
+            {todo.title}
+            <button onClick={() => moveTodo(todo.id)}>Complete</button>
           </div>
         ))}
       </div>
       <div className="flex-item">
         <h1>complete todos</h1>
         {completedTodos.map(todo => (
-          <div>{todo.title}
-          <button onClick={() => moveTodo(todo.id)}>Undo</button>
+          <div key={todo.id} className="todo-item">
+            <span>{getUserName(todo.userId)}: </span>
+            {todo.title}
+            <button onClick={() => moveTodo(todo.id)}>Undo</button>
           </div>
         ))}
+      </div>
       </div>
     </div>
   )
